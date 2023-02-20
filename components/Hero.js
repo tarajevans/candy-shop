fae8799149ed95065e53f97a48206ed56422c25c
Tara Evans <tarajevans@hotmail.com>
2023-02-20 12:00:04 -0500

    added Hero

---
+++ components/Hero.js
@@ -0,0 +1,26 @@
+const Hero =() => {
+    return <div className="flex flex-col md:flex-row md:mb-12">
+        <div className="md:w-1/2">
+            <div className="text-4xl xl:text-5xl leading normal py-8 font-fredoka">
+                Welcome to <br />
+                The <span className="text-orange-300"> Candy </span> Shop
+            </div>
+            <div className="text-xl">
+                Shop artisan candy, gourmet dark chocolates, milk chocolates and white chocolates, and candy gifts
+            </div>
+            <div>
+                <a>
+                    <div className="text-black m-9 inline-flex bg-white p-4 rounded-full font-bold px-7">
+                        Shop now
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div className="flex md:w-1/2 align-middle justify-center pt-10">
+            <img className="w-56 md:w-72 xl:w-80" src='./lollipop.png' alt='lollipop'>
+            </img>
+        </div>
+    </div>
+
+}
+export default Hero
