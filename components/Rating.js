ffecc4cf4900740276525df46b2aeb3133028c07
Tara Evans <tarajevans@hotmail.com>
2023-02-22 20:39:54 -0500

    created Ratings component

---
+++ components/Rating.js
@@ -0,0 +1,15 @@
+import Image from "next/image"
+
+const Rating = () => {
+  return (
+    <div>
+        <Image src='/star-icon.svg' alt='star icon' width={20} height={20} />
+        <Image src='/star-icon.svg' alt='star icon' width={20} height={20} />
+        <Image src='/star-icon.svg' alt='star icon' width={20} height={20} />
+        <Image src='/star-icon.svg' alt='star icon' width={20} height={20} />
+        <Image src='/star-gray.svg' alt='star icon' width={20} height={20} />
+    </div>
+  )
+}
+
+export default Rating
