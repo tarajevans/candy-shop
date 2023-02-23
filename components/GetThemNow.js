c1768f6455bdf0c6a3adf50a01199a4a57049f6f
Tara Evans <tarajevans@hotmail.com>
2023-02-22 20:34:44 -0500

    built Get Them Now section

---
+++ components/GetThemNow.js
@@ -0,0 +1,59 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const GetThemNow = () => {
+    return(
+<section className="mb-14">
+    <div className="px-3 md:px-7 w-100 mx-auto xl:w-4/6">
+        <div className="relative flex justify-center items-center">
+            <span className='w-full xs:block md:hidden'>
+                <Image
+                src='/banner-sm.png'
+                alt='get now'
+                width='344'
+                height='490'
+                className="mx-auto"
+                />
+            </span>
+            <span className='w-full hidden md:block xl:hidden'>
+                <Image
+                src='/banner-md.png'
+                alt='get now'
+                width='704'
+                height='332'
+                className="mx-auto"
+                />
+            </span>
+            <span className='w-full hidden xl:block'>
+                <Image
+                src='/banner-lg.png'
+                alt='get now'
+                width='1140'
+                height='400'
+                className="mx-auto"
+                />
+            </span>
+            <div className="top-0 absolute w-full h-full">
+                <div className="flex flex-col pt-16 md:pt-0 md:justify-center items-center h-full text-center">
+                    <div className="font-fredoka text-3xl font-bold text-custom-purple">
+                        Get Them Now
+                        </div>
+                    <div className="text-sm mt-2 font-semibold text-custom-purple leading-8 tracking-wide">
+                        Candy, lollipops, gum starts from <span className="text-red-500">$9.99</span>
+                    </div>
+                    <div className="mt-8">
+                        <Link href='' className="bg-red-500 px-6 text-white py-3 text-sm font-semibold rounded-full box-content">
+                            Read More
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+    )
+}
+
+export default GetThemNow
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Layout from "@/components/Layout"
 import BestSellers from "@/components/BestSellers"
+import GetThemNow from "@/components/GetThemNow"
 
 export default function Home() {
   return (
 
       <Layout criteria={true}>
       <BestSellers />
+      <GetThemNow />
       </Layout>
   )
 }
