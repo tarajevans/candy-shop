07d5ddc1ae5a15e7cc05f2b40f0eb8c43ed14690
Tara Evans <tarajevans@hotmail.com>
2023-02-24 11:47:56 -0500

    built shopping cart from cart components

--- pages/cart.js
+++ pages/cart.js
@@ -1,10 +1,46 @@
 import RecomendedProducts from "@/components/RecomendedProducts"
+import Layout from "@/components/Layout"
+import Promocode from "@/components/cart/Promocode"
+import Subtotal from "@/components/cart/Subtotal"
+import CartItem from "@/components/cart/CartItem"
 
+const orderData = [
+  { name: ' Jelly Candy', image: '/14.png', inStock: true, price: 19.23 },
+  { name: ' Sugar Gum', image: '/15.png', inStock: true, price: 13.45 },
+]
 const Cart = () => {
   return (
-    <div>
-        <RecomendedProducts />
-    </div>
+    <Layout>
+      <div className="bg-custom">
+        <div className="px-3 w-100 mx-auto xl:w-4/6">
+          <div className="grid grid-cols-1 lg:grid-cols-3 mb-24 gap-3">
+            <div className="col-span-2">
+              <div className="border-2 rounded-2xl border-slate-400 p-5 mb-4 md:mb-0 mr-0 bg-white">
+                <div className="mb-6 font-fredoka font-semibold font-24 text-custom-purple ">
+                  Products
+                </div>
+                {orderData.map((item, index) => {
+                  return (
+                    <CartItem 
+                      key={index} 
+                      name={item.name} 
+                      image={item.image} 
+                      inStock={item.inStock} 
+                      price={item.price} 
+                    />
+                  )
+                })}
+              </div>
+            </div>
+            <div>
+              <Promocode />
+              <Subtotal />
+            </div>
+          </div>
+          <RecomendedProducts />
+        </div>
+      </div>
+    </Layout>
   )
 }
 
