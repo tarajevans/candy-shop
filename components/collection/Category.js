1c63507e52d349bcc99eab937af707b24924e3aa
Tara Evans <tarajevans@hotmail.com>
2023-02-24 12:40:44 -0500

    built Category component for Collection page

---
+++ components/collection/Category.js
@@ -0,0 +1,30 @@
+const categoryMenu = [
+    { item: 'Sugar Candy', qty: 32 },
+    { item: 'Sugar Lollypop', qty: 123},
+    { item: 'Chewing Gum', qty: 56 },
+    { item: 'Jelly Candy', qty: 15 },
+    { item:  "M&M's", qty: 3 },
+]
+
+const Category = () => {
+    return <>
+    <div className="font-fredoka text-xl font-bold">Catgory</div>
+    <div className="flex flex-col gap-3 mt-3">
+        {categoryMenu.map((candy, index) => {
+            return (
+                <div className="flex justify-between font-poppins font-medium text-custom-purple-lite" key={index}>
+                    <span>
+                        <span className="text-red-500 mr-2">•</span>
+                        {candy.item}
+                    </span>
+                    <span className="text-red-500 h-[22px] font-extrabold bg-custom mt-1 rounded-full px-2 text-sm">
+                        {candy.qty}
+                    </span>
+                </div>
+            )
+        })}
+    </div>
+    </>
+
+
+}
